a14f58f26052113f9f3462219e5785a4d491c32d
leha-code <pezleha@gmail.com>
2022-02-01 18:53:32 -0500

    Basic program

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+MarketPi (codename)
+
+This is a client for downloading worlds for Minecraft Pi Edition.
+
+
 MIT License
 
 Copyright (c) 2022 mcpiscript
@@ -19,3 +25,44 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+
+"""
+
+import sys
+from urllib.request import urlopen
+import tempfile
+
+
+
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+
+def url_to_qpixmap(url: str) -> QPixmap:
+    image = None
+    with tempfile.NamedTemporaryFile(suffix='_tempcache', prefix='marketpi_') as file:
+        file.write(urlopen(url).read())
+        image = QPixmap(file.name)
+    return image 
+
+# Subclass QMainWindow to customize your application's main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+        
+        label = QLabel()
+        label.setPixmap(url_to_qpixmap("https://github.com/mcpiscript/marketpi-repo/raw/main/worlds/NRC_History/NRC_History.png"))
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window.
+        self.setCentralWidget(label)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
